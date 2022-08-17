2407050a7e7e095784300370de092c5a6dce838c
Aidan Sherry <aidansherry24@gmail.com>
2022-08-17 16:17:12 -0400

    Created form to submit a new poll

---
+++ app/controllers/api/v1/polls_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::PollsController < ApiController
+    def index
+        render json: { polls: Poll.all }
+    end
+
+    def create
+        poll = Poll.new(poll_params)
+        if poll.save
+            render json: poll
+        else
+            render json: { errors: poll.errors.full_messages }, status: 400
+        end
+    end
+
+    def show
+        poll = Poll.find(params[:id])
+        render json: poll
+    end
+
+    private
+
+    def poll_params
+        params.require(:poll).permit(:title, :body, :option_1, :option_2)
+    end
+
+end
